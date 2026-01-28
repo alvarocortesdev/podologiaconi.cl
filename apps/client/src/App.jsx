0fc6d86c8dd1807a5f35a2c9350cab820c3709b0
Alvaro Cortés <alvaro.cortes.dev@outlook.com>
2026-01-28 14:02:28 -0300

    fix: separate admin layout from public layout

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,23 +1,35 @@
 // client/src/App.jsx
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './layouts/Layout';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import Admin from './pages/Admin';
-import { ConfigProvider } from './context/ConfigContext';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Admin from "./pages/Admin";
+import { ConfigProvider } from "./context/ConfigContext";
 
 function App() {
   return (
     <ConfigProvider>
       <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/servicios" element={<Services />} />
-            <Route path="/admin" element={<Admin />} />
-          </Routes>
-        </Layout>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
+          <Route
+            path="/servicios"
+            element={
+              <Layout>
+                <Services />
+              </Layout>
+            }
+          />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
       </BrowserRouter>
     </ConfigProvider>
   );
